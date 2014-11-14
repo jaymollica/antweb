374f59d566b6fa03c61bf8f76b6f5b05e5194459
Jay Mollica <jmollica@calacademy.org>
2014-11-14 11:40:42 -0800

    specimen chart in demos

--- demo/madagascar-species/index.php
+++ demo/madagascar-species/index.php
@@ -1,5 +1,7 @@
 <?php
 
+//this file is just meant for ad hoc processing of large data sets, eventually I'd like to turn it into OO classes once we figure out some typical use cases
+
 $specimen = file_get_contents('madagascar1970-2010.json');
 
 $specimen = json_decode($specimen);
@@ -10,6 +12,7 @@ foreach($specimen->specimens AS $s) {
 	$species[$i]['date'] = date("Y",strtotime($s->datecollected));
 	$species[$i]['name'] = $s->scientific_name;
 	$species[$i]['collectioncode'] = $s->fieldNumber;
+	$species[$i]['code'] = $s->catalogNumber;
 	$i++;
 }
 
@@ -17,10 +20,12 @@ $distincts = array();
 $collection_events = array();
 $by_year = array();
 $events = array();
+$specimens = array();
 
 for($i = 1970; $i <= 2010; $i++) {
 	$by_year[$i] = array();
 	$events[$i] = array();
+	$specimens[$i] = array();
 	foreach($species AS $s) {
 		if($s['date'] == $i) {
 			if(!in_array($s['name'], $distincts)) {
@@ -31,10 +36,12 @@ for($i = 1970; $i <= 2010; $i++) {
 				array_push($collection_events, $s['collectioncode']);
 				array_push($events[$i], $s['collectioncode']);
 			}
+			array_push($specimens[$i], $s['code']);
 		}
 	}
 }
 
+/*
 $data = array();
 $i = 0;
 $total = 0;
@@ -66,6 +73,19 @@ foreach($events AS $year => $val) {
 $data = json_encode($data);
 
 print $data;
+*/
 
+//collection events
+$data = array();
+$i = 0;
+foreach($specimens AS $year => $val) {
+	$data[$i]['year'] = $year;
+	$data[$i]['count'] = count($val);
+	$i++;
+}
+
+$data = json_encode($data);
+
+print $data;
 
 ?>
